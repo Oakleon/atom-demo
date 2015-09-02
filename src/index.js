05bfb23fafaa1dbe35be368fbaf597752b4439a8
Chris Hiestand <chrishiestand@gmail.com>
2015-09-01 17:45:35 -0700

    log errors - await was swallowing them

--- src/index.js
+++ src/index.js
@@ -9,18 +9,22 @@ async function fetch() {
 }
 
 function report({cpu, memory, load, disk = "No Disk"}) {
+
     let cpu_values  = _Lodash.values(cpu);
     let cpu_average = cpu_values.reduce((a,b) => { return a+b; }) / cpu_values.length;
 
     console.log(`cpu: ${cpu_average}%`);
-
     console.log(`memory: ${memory.used}B used, ${memory.free}B free`);
     console.log(`load averages: ${load['1m']},${load['5m']},${load['15m']}`);
     console.log(`disk: ${disk}`);
 }
 
 async function main() {
-    let data = await fetch();
+    let data = await fetch()
+    .catch((err) => {
+        console.error(err);
+    });
+
     report(data);
 }
 
